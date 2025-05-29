79b43fc758facf46ac6ffa0a1ca40900f3e85ae2
ilyalozhecnikov <ilozhechnikov@aristeksystems.com>
2025-05-29 22:28:27 +0300

    Add Swagger and update api documentation

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { ConfigService } from '@nestjs/config';
 import { useContainer } from 'class-validator';
 import { UsersModule } from './users/users.module';
 import { CustomValidationPipe } from './common/custom-validation.pipe';
+import * as swaggerUi from 'swagger-ui-express';
+import * as YAML from 'yamljs';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +16,10 @@ async function bootstrap() {
   useContainer(app.select(UsersModule), {
     fallbackOnErrors: true,
   });
+
+  const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+
+  app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   const configService = app.get(ConfigService);
 
